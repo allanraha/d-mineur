5ed3a34f4842ae9acb8e639a92e0cf741d96ac36
allanraha <104799181+allanraha@users.noreply.github.com>
2022-05-23 23:19:11 +0200

    Update demineur.c

--- demineur.c
+++ demineur.c
@@ -3,34 +3,54 @@
 #include <time.h>
 
 
-char JEU[10][10];
-char mine[10][10];
+int JEU[20][20];
+char mine[20][20];
 int i,j;
+int l,c,nbmines;
 
+void Init(int l,int c){
+      for(i=0;i<l;i++){
+        for(int j=0;j<c;j++){
+            JEU[i][j]='~';
+            mine[i][j]='0';
+        }
+    }
+  
+}
 
-void GRILLEdeb(){
-    printf("   A B C D E F G H I J\n");
-    for(i=0;i<10;i++){
+void GRILLE(int lig,int col){
+    char a;
+    printf("   ");
+    for(a='A';a<'A'+col;a++){
+        printf("%c ",a);
+    }
+    printf("\n");
+    for(i=0;i<lig;i++){
+      if(i<10){
         printf("%d |",i);
-        for(j=0;j<10;j++){
+      }
+      else{
+        printf("%d|",i);
+      }
+      
+        for(j=0;j<col;j++){
             printf("%c|",JEU[i][j]);
         }
         printf("\n");
     }
 }
 
-void MINEdeb(){
-	int c=10;
+void MINE(int nbmines,int lig,int col){
 	int k=0;
 	int l=0;
-	for(i=0;i<c;i++){
-        l=rand()%10;
-        k=rand()%10;
+	for(i=0;i<nbmines;i++){
+        l=rand()%lig;
+        k=rand()%col;
         if(mine[l][k]=='0'){
             mine[l][k]='M';
         }
         else{
-            c++;
+            nbmines++;
         }
     }
 }
@@ -170,17 +190,14 @@ int MineAutour(int l,int c){
 				N=N+1;
 				}
 			if(mine[l-1][c-1]=='M'){
-			N=N+1;
+				N=N+1;
 				}
 			if(mine[l-1][c]=='M'){
 				N=N+1;
 				}
 			if(mine[l-1][c+1]=='M'){
 				N=N+1;
-				}
-			if(mine[l][c-1]=='M'){
-				N=N+1;
-				}
+				}			
 			if(mine[l][c+1]=='M'){
 				N=N+1;
 				}
@@ -195,61 +212,72 @@ int MineAutour(int l,int c){
 				}
 			return N;
 		}
-	
-		
-		
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
 
+void choixdifficulte(){
+  int diff;
+  printf("0 = debutant , 1 = intermédiare\n");
+  printf("Choisir la difficulte: ");
+  scanf("%d",&diff);
+  switch(diff){
+    case 0:
+      l=10;
+      c=10;
+      nbmines=10;
+    break;
+    
+    case 1:
+      l=17;
+      c=17;
+      nbmines=40;
+    break;
+  }
 
+    
+}
 
-
+void jouer(){
+  int choix_c,choix_l;  
+ 	printf("quel case ?");
+  scanf("%d",&choix_l);
+  scanf("%d",&choix_c);
+  JEU[choix_l][choix_c]=MineAutour(choix_l,choix_c)+'0';
+  printf("\n\n");
+ 	for(i=0;i<l;i++){
+    for(j=0;j<c;j++){
+      printf(" %c",mine[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+  GRILLE(l,c);
+}
 
 
 
 int main(){
+  int aut;
+  char p;
+  srand(time(NULL));
+  
+  
+  choixdifficulte();
+  Init(l,c);
 
-    int i,j;
-    int aut;
-    srand(time(NULL));
-    for(i=0;i<10;i++){
-        for(int j=0;j<10;j++){
-            JEU[i][j]='~';
-            mine[i][j]='0';
-        }
-    }
-    
-    
-	
-       
-    
-    GRILLEdeb();
-    MINEdeb();
-    aut=MineAutour(0,0);
-    printf("\n\n");
-    for(i=0;i<10;i++){
-        for(j=0;j<10;j++){
-            printf(" %c",mine[i][j]);
-        }
-        printf("\n");
-    }
-    printf("il y a %d mines autour de la case 0:0",aut);
-    
-    return 0;
+	MINE(nbmines,l,c);
+  GRILLE(l,c);
+  
+
+for(i=0;i<100;i++){
+  jouer();
 }
+  
+
+   
+    
+  return 0;
+}
+
 
 
