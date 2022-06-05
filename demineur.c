9676036b675b94c634c20993eb0d991e46dce9d5
Poupouski <105277484+Poupouski@users.noreply.github.com>
2022-06-05 21:06:12 +0200

    Update demineur.c

--- demineur.c
+++ demineur.c
@@ -1,46 +1,46 @@
-#include "main.h"
+#include "demineur.h"
 
 int main(){
-  int condvictoire = 0; 
+  int cond_victoire = 0; 
   int compt=0;
   int tour;
   int rejouer=1;
   char p;
-  char JEU[30][30];
+  char jeu[30][30];
   char mine[30][30];
   int i,j;
   int l,c,nbmines;
-  int conddéfaite=0;
+  int cond_defaite=0;
   
   srand(time(NULL));   
 
   while(rejouer==1){
     choixdifficulte(&l,&c,&nbmines);   
-    Init(mine,JEU,l,c);          
-    MINE(mine,nbmines,l,c);  
-    GRILLE(JEU,l,c);        
-    condvictoire = (l*c)-nbmines;                      
+    init(mine,jeu,l,c);          
+    placermine(mine,nbmines,l,c);  
+    grille(jeu,l,c);        
+    cond_victoire = (l*c)-nbmines;                      
     time_t temps1 = time(NULL);
     time_t temps2;
-    while(compt!=condvictoire && conddéfaite==0){ 
+    while(compt!=cond_victoire && cond_defaite==0){ 
       compt=0;                                    
       tour=choixtour();                           
       if(tour==0){                               
-        jouer(mine,JEU,l,c,&conddéfaite);
+        jouer(mine,jeu,l,c,&cond_defaite);
       }                      
       if(tour==1){             
-        placerdrapeau(JEU,l,c);
+        placerdrapeau(jeu,l,c);
       }
       for(i=0;i<l;i++){
         for(j=0;j<c;j++){
-          if(JEU[i][j]!='~' && JEU[i][j]!='X'){
+          if(jeu[i][j]!='~' && jeu[i][j]!='X'){
             compt=compt+1;          
           }                           
         }  
       }
     
     }
-    if(compt==condvictoire){         
+    if(compt==cond_victoire){         
       printf("Bravo vous avez gagnez !\n");
       temps2 = time(NULL);
       printf("Votre temps de jeu: %lf secondes\n",difftime(temps2, temps1));
@@ -55,7 +55,7 @@ int main(){
         r = scanf("%c", &c);               
       }while(r==1 && c!='\n');         
     }while(rejouer!=0 && rejouer!=1);  
-    conddéfaite=0;                   
+    cond_defaite=0;                   
     compt=0;
   }
   return 0;
